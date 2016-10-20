fd803f9313221ec2e1c966ca1314f8185719b465
dcat <dcat@iotek.org>
2016-10-20 17:39:36 +0200

    add cat.s primer

---
+++ cat.s
@@ -0,0 +1,5 @@
+@ get and parse args
+@ print files;
+@ 	stat() file/get size
+@ 	read() file
+@ 	write() to stdout
--- makefile
+++ makefile
@@ -2,3 +2,4 @@ LDFLAGS = --entry _start -nostdlib -static
 ASFLAGS = -g
 
 echo: echo.s
+cat: cat.s
